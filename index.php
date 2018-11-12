c07962c6dba0214b93688f16a5047b95dffadd11
Christopher Gillispie <christophers-github@users.noreply.github.com>
2018-11-12 16:47:15 -0600

    Added Open Weather API functionality

--- index.php
+++ index.php
@@ -1,31 +1,20 @@
+<!-- 
+CIS 131 Group Weather API Assignment
+Rose Haney, Chris Gillispie
+-->
 <!DOCTYPE html>
-<? 
-$url = "https://api.darksky.net/forecast/4cfc843429d7e1c530baaccdcec3e23d/37.8267,-122.4233";
-$getweather = simplexml_load_file($url);
-$gethumidity = $getweather->list->item->humidity['value'];
-$gettemp = $getweather->list->item->temperature['value'];
-?>
-
-
 <html lang = "en">
     <head>
         <meta charset="utf-8"/>
-        <title>Next 5 Launches</title>
+        <title>Group Weather API</title>
         <link rel ="stylesheet" href="main.css"/>
+        <script src="script.js"></script>
     </head>
-      
     <body>
         <heading>Weather</heading>
-
-        <script>
-            console.log($gethumidity');
-        </script>
-
-        <ul>
-            
-            <li>Humidity: <? echo($gethumidity); ?>%</li>
-            <li>Temperature: <? echo($gettemp); ?> &deg;F</li>
-
+        <ul>          
+            <li>Humidity: <span id="humiditySpan"></span>%</li>
+            <li>Temperature: <span id="temperatureSpan"></span> &deg;F</li>
         </ul>
     </body>
 </html>
---
+++ script.js
@@ -0,0 +1,30 @@
+/*  
+CIS131 Group Weather API Assignment
+Rose Haney, Chris Gillispie
+*/
+
+// confirmed file is loaded
+//console.log('Worked');
+var weatherReport;
+var httpRequest = new XMLHttpRequest();
+
+function getWeather() {
+   //console.log("Got here");
+   httpRequest.open("get","getJSON.php");
+   httpRequest.send(null);
+   httpRequest.onreadystatechange = aFunction;
+
+}
+
+function aFunction() {
+  if (httpRequest.readyState == 4 && httpRequest.status == 200)
+  {
+    var weatherReport = httpRequest.responseText;
+    console.log(weatherReport);
+    var jsObject = JSON.parse(weatherReport);
+    console.log(jsObject.currently.apparentTemperature);
+    document.getElementById("humiditySpan").innerHTML = jsObject.currently.humidity;
+    document.getElementById("temperatureSpan").innerHTML = jsObject.currently.temperature;
+  }
+}
+getWeather();
